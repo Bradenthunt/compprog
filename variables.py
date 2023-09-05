cc6c5022237055e4b4e8f7eb37966d057fcfb318
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2023-09-05 13:28:13 -0600

    variables and a function

---
+++ variables.py
@@ -0,0 +1,17 @@
+my_name = "Mr Hunt"
+test_score = 100
+was_lunch_tasty = True
+b_day_classes = ["Gym", "Math", "Computer Programming", "Web Development"]
+
+print(f"Hi, my name is {my_name}. I just scored {test_score} on my last test. My classes today are {b_day_classes}.")
+
+def add_two_numbers1():
+    print(1 + 2)
+
+def add_two_numbers2(num1, num2):
+    print(num1 + num2)
+
+user_number1 = int(input("Give me a number: "))
+user_number2 = int(input("Give me a 2nd number: "))
+
+add_two_numbers2(user_number1, user_number2)
