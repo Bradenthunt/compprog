a800a5949282b51933fe8818a90a247b1ed7c649
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2023-09-28 13:30:28 -0600

    for loops

--- forloops.py
+++ forloops.py
@@ -1,3 +1,11 @@
 word = 'paradigm'
 for i in word:
     print(i)
+
+list1 = [5,10,15,20]
+list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
+
+for i in list1:
+    for j in list2:
+        print(i)
+        print(j)
