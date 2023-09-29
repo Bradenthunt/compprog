2010d8813c7a0c97b02ac300802fa8fc1dc334d8
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2023-09-29 12:37:07 -0600

    flags

--- forloops.py
+++ forloops.py
@@ -7,5 +7,20 @@ list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
 
 for i in list1:
     for j in list2:
+        print(i, j)
+
+numbers = [12,3,56,67,89,90]
+sum = 0
+
+for i in numbers:
+    sum += i
+
+print(sum)
+
+vehicles = ['Car','Cycle','Bus','Tempo']
+
+for i in vehicles:
+    if i == "Bus":
+        break
+    else:
         print(i)
-        print(j)
