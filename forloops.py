75422110e70401e1ebbaba3531b5beaf3bc86ce1
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2023-10-10 13:30:36 -0600

    worked on coding challenges

--- forloops.py
+++ forloops.py
@@ -24,3 +24,43 @@ for i in vehicles:
         break
     else:
         print(i)
+
+list1 = ['Mango','Banana','Orange']
+list2 = []
+
+for i in list1:
+    list2.append(i)
+
+print(list2)
+
+animal_list = ["cat", "dog", "tiger", "cow"]
+for i in animal_list:
+    print(i)
+
+# how_deep = int(input("How deep into the Fibonacci would you like to go? "))
+
+# def fibonacci(num):
+#     for i in range(num):
+#         x = i
+#         y = x
+#         z = x + y
+#         x = z
+#         print(x)
+
+# fibonacci(how_deep)
+
+user1 = input("User1: Rock, paper or scissors? ")
+user2 = input("User2: Rock, paper or scissors? ")
+
+winning_key = {
+    
+}
+
+if (user1 == "Rock" or user1 == "rock") and (user2 == "paper" or user2 == "Paper"):
+    print("User2 wins!")
+
+if (user1 == "Scissors" or user1 == "scissors") and (user2 == "paper" or user2 == "Paper"):
+    print("User1 wins!")
+
+if (user1 == "Paper" or user1 == "paper") and (user2 == "paper" or user2 == "Paper"):
+    print("User2 wins!")
