894c671e1287fe7bccf3cd232628e8a3ca61f458
Braden Hunt <bradenthunt@Bradens-MacBook-Pro.local>
2023-10-12 13:30:15 -0600

    finished fibonacci and rps

--- forloops.py
+++ forloops.py
@@ -1,66 +1,70 @@
-word = 'paradigm'
-for i in word:
-    print(i)
+# word = 'paradigm'
+# for i in word:
+#     print(i)
 
-list1 = [5,10,15,20]
-list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
+# list1 = [5,10,15,20]
+# list2 = ['Tomatoes','Potatoes','Carrots','Cucumbers']
 
-for i in list1:
-    for j in list2:
-        print(i, j)
+# for i in list1:
+#     for j in list2:
+#         print(i, j)
 
-numbers = [12,3,56,67,89,90]
-sum = 0
+# numbers = [12,3,56,67,89,90]
+# sum = 0
 
-for i in numbers:
-    sum += i
+# for i in numbers:
+#     sum += i
 
-print(sum)
+# print(sum)
 
-vehicles = ['Car','Cycle','Bus','Tempo']
+# vehicles = ['Car','Cycle','Bus','Tempo']
 
-for i in vehicles:
-    if i == "Bus":
-        break
-    else:
-        print(i)
+# for i in vehicles:
+#     if i == "Bus":
+#         break
+#     else:
+#         print(i)
 
-list1 = ['Mango','Banana','Orange']
-list2 = []
+# list1 = ['Mango','Banana','Orange']
+# list2 = []
 
-for i in list1:
-    list2.append(i)
+# for i in list1:
+#     list2.append(i)
 
-print(list2)
+# print(list2)
 
-animal_list = ["cat", "dog", "tiger", "cow"]
-for i in animal_list:
-    print(i)
+# animal_list = ["cat", "dog", "tiger", "cow"]
+# for i in animal_list:
+#     print(i)
 
 # how_deep = int(input("How deep into the Fibonacci would you like to go? "))
+# x = 0
+# y = 1
+# z = y  
+# count = 1
+ 
+# while count <= how_deep:
+#     print(f"{z} ")
+#     count += 1
+#     x, y = y, z
+#     z = x + y
 
-# def fibonacci(num):
-#     for i in range(num):
-#         x = i
-#         y = x
-#         z = x + y
-#         x = z
-#         print(x)
-
-# fibonacci(how_deep)
 
 user1 = input("User1: Rock, paper or scissors? ")
 user2 = input("User2: Rock, paper or scissors? ")
 
-winning_key = {
-    
-}
+if user1 == "Rock" or user1 == "rock":
+    if user2 == "paper" or user2 == "Paper":
+        print("User2 wins!")
+    elif user2 == "scissors" or user2 == "Scissors":
+        print("User1 wins!")
+    elif user2 == "Rock" or user2 == "rock":
+        print("You chose the same. Try again!")
 
-if (user1 == "Rock" or user1 == "rock") and (user2 == "paper" or user2 == "Paper"):
-    print("User2 wins!")
-
-if (user1 == "Scissors" or user1 == "scissors") and (user2 == "paper" or user2 == "Paper"):
-    print("User1 wins!")
-
-if (user1 == "Paper" or user1 == "paper") and (user2 == "paper" or user2 == "Paper"):
-    print("User2 wins!")
+if user1 == "Paper" or user1 == "paper":
+    if user2 == "paper" or user2 == "Paper":
+        print("You chose the same. Try again!")
+    elif user2 == "scissors" or user2 == "Scissors":
+        print("User2 wins!")
+    elif user2 == "Rock" or user2 == "rock":
+        print("User1 wins!")
